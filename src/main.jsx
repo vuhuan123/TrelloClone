ba3a47057c0551bd036dbf5dd50872723eb38a52
Vupv <vupv38@gmail.com>
2024-09-02 15:20:39 +0700

    cssbaseline

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import CssBaseline from '@mui/material/CssBaseline';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <CssBaseline />
     <App />
   </StrictMode>,
 )
